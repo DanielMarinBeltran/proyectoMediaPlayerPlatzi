a5a8f3148b3f71a9a2ab0fb54eb746e1e30c7729
Daniel Marin <danielmarin998@hotmail.com>
2021-08-27 17:26:07 -0500

    added the first plugin to the project.

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,14 @@
-import MediaPlayer from './mediaPlayer';
+import MediaPlayer from './mediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js'
+
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video});
+const player = new MediaPlayer({ el: video, plugins: [
+    //new AutoPlay()
+]});
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const button = document.querySelector('playButton');
+playButton.onclick = () => player.togglePlay();
+
+const muteButtom = document.querySelector('#muteButton');
+muteButtom.onclick = () => player.toggleMute();
