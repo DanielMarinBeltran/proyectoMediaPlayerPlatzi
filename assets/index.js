30fae4bbf1bbf6c770c298d52cf4b85fa5499cef
Daniel Marin <danielmarin998@hotmail.com>
2021-08-31 14:50:13 -0500

    implemente getter and setter y mas ejercicios

--- assets/index.js
+++ assets/index.js
@@ -3,9 +3,7 @@ import AutoPlay from './plugins/AutoPlay.js'
 
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [
-    //new AutoPlay()
-]});
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()]});
 
 const button = document.querySelector('playButton');
 playButton.onclick = () => player.togglePlay();
