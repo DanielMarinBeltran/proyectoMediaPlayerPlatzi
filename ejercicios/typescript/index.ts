54e6f94f8b87f76d39173482f2fba208c5fa6206
Daniel Marin <danielmarin998@hotmail.com>
2021-09-04 13:32:08 -0500

    interfaces en TS

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -27,14 +27,14 @@ peopleAndNumbers.push(9000);
 
 // Enum
 
-enum Color {
-    Rojo = "Rojo",
-    Verde = "Verde",
-    Azul = "Azul",
-};
+// enum Color {
+//     Rojo = "Rojo",
+//     Verde = "Verde",
+//     Azul = "Azul",
+// };
 
-let colorFavorito: Color = Color.Rojo;
-console.log(`Mi color favorito es ${colorFavorito}`);
+// let colorFavorito: Color = Color.Rojo;
+// console.log(`Mi color favorito es ${colorFavorito}`);
 
 // Any
 
@@ -45,3 +45,62 @@ comodin = { type: 'Wildcard' };
 // Object
 
 let someObject:object = { type: 'Wildcard' };
+
+// Function
+
+function add(a: number, b: number): number{
+    return a + b;
+};
+
+const sum = add(4, 6);
+
+function createAdder (a: number) : (number) => number {
+    return function(b: number){
+        return b + a;
+    };
+};
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName: string, lastName: string = ''): string {
+    return `${firstName} ${lastName}`;
+};
+
+const richard = fullName("Richard");
+
+console.log(richard);
+
+// Interfaces
+
+enum Color {
+    Amarillo = "Amarillo",
+    Morado = "Morado"
+}
+
+interface Rectangulo {
+    ancho: number;
+    alto: number;
+    color ?: Color;
+};
+
+let rect: Rectangulo = {
+    ancho:4,
+    alto:6,
+    color: Color.Amarillo,
+};
+
+function area ( r:Rectangulo){
+    return r.alto * r.ancho;
+};
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+console.log(rect.toString());  // [Object Object]
+
+rect.toString = function (){
+    return this.color ? `Un rectangulo ${this.color}` : `Un rectangulo`;
+}
+
+console.log(rect.toString());
